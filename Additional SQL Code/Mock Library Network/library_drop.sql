0069b8a7692bfe7453a4e109e94283ca7b71bb80
Peter Wotherspoon <108731810+peterw-github@users.noreply.github.com>
2023-02-25 10:10:07 +1100

    fixed spacing

--- Additional SQL Code/Mock Library Network/library_drop.sql
+++ Additional SQL Code/Mock Library Network/library_drop.sql
@@ -1,16 +1,16 @@
 --library_drop.sql
-
 --author name: Peter Wotherspoon Bustsamante
 
-/* 
 
+
+
+/* 
 Dropping several tables of the library networks relational database.
 
 The order in which tables are dropped, is set so that foreign 
 keys aren't removed prematurely,(which would otherwise stop other 
 tables from being dropped later on, unless we're using CASCADE
 CONSTRAINTS, which we're avoiding in this case).
-
 */ 
 
 
